bd85e26df237535491bbae59ada011deb11c2c93
Swathikrishna-0 <ssspriyamoru@gmail.com>
2023-12-26 18:30:20 +0530

    Development completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch, Redirect} from 'react-router-dom'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import LoginPage from './components/LoginPage'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+import Popular from './components/Popular'
+import Account from './components/Account'
+import SearchFilter from './components/SearchResult'
+import NotFound from './components/NotFound'
+import MovieDetailView from './components/MovieDetailView'
+import ProfilePage from './components/ProfilePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css'
 
-export default App;
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginPage} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/movies/:id" component={MovieDetailView} />
+    <ProtectedRoute exact path="/search" component={SearchFilter} />
+    <ProtectedRoute exact path="/popular" component={Popular} />
+    <ProtectedRoute exact path="/Account" component={Account} />
+    <ProtectedRoute exact path="/profilepage" component={ProfilePage} />
+
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
